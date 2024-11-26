6a0f5460e8306dd346817557098407fe87e4a253
Jorge Mario Pino Romero <jmpromero98@gmail.com>
2024-11-25 21:42:48 -0500

    Se agrega la funcion int_resta y se actualiza el main de la app

--- app.py
+++ app.py
@@ -2,6 +2,11 @@
 def int_sum(val1: int,val2: int):
     return val1+val2
 
+def int_resta(val1: int,val2: int):
+    return val1+val2
+
 if __name__ == "__main__":
-    res = int_sum(2,3)
-    print(f'El resultado de la suma es {res}')
+    res1 = int_sum(2,3)
+    res2 = int_resta(2,3)
+    print(f'El resultado de la operacion de suma es {res1}')
+    print(f'El resultado de la operacion de esta es {res2}')
